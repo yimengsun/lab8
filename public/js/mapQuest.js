08e6b566a6a8173b48768fb53c5e5f32c6247a78
Yimeng Sun <yms001@ucsd.edu>
2019-02-28 10:48:52 -0800

    add map camera

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'V9b300IEHgTTvwdepiWwbKMBT05j01u4';
+
+	var map = L.mapquest.map('map',{
+		center:[32.88,-117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88,-117.236]).addTo(map);
 }
